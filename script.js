5684a1329ce4409f19737af8e623ca6db90e9a47
dbeslija <dzemila.beslija@gritacademy.se>
2026-04-07 20:46:28 +0200

    grupparbete tisdag

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
-console.log("test");
 
 const questions = [
     {
         id: 1,
-        question: "1. Vad används let till i JavaScript?",
+        question: "Vad används let till i JavaScript? 1",
         answers: ["För att deklarera en konstant", "För att deklarera en variabel", "För att skapa en funktion", "För att skriva ut text"],
-        correct: 1
+        correct: 1,
+
     },
     {
         id: 2,
-        question: "2. Vad returnerar typeof 'Hello'?",
+        question: "Vad returnerar typeof 'Hello'? 2",
         answers: ["string", "text", "object", "char"],
         correct: 0
     },
     {
         id: 3,
-        question: "3. Hur skriver man en kommentar på en rad i JavaScript?",
+        question: "Hur skriver man en kommentar på en rad i JavaScript? 3",
         answers: ["<!-- kommentar -->", "# kommentar", "// kommentar", "** kommentar **"],
         correct: 2
     },
     {
         id: 4,
-        question: "4. Vad blir resultatet av '2' + 2?",
+        question: "Vad blir resultatet av '2' + 2? 4",
         answers: ["4", "22", "NaN", "undefined"],
         correct: 1
     },
     {
         id: 5,
-        question: "5. Vad är true i JavaScript?",
+        question: "5. Vad är true i JavaScript? 5",
         answers: ["En tal", "En text", "En boolean", "En array"],
         correct: 2,
     },
     {
         id: 6,
-        question: "6. Vad är en array i JavaScript?",
+        question: "6. Vad är en array i JavaScript? 6",
         answers: ["En funktion", "En lista med värden", "Ett tal", "En loop"],
         correct: 1
     },
     {
         id: 7,
-        question: "7. Vad är en variabel?",
+        question: "7. Vad är en variabel? 7",
         answers: ["Ett fast värde som aldrig ändras", "Ett sätt att lagra data", "En typ av kommentar", "En operator"],
         correct: 1
     },
     {
         id: 8,
-        question: "8. Vad betyder === i JavaScript?",
+        question: "8. Vad betyder === i JavaScript? 8",
         answers: ["Tilldelning", "Jämförelse av värde", "Jämförelse av värde och typ", "Inte lika med"],
         correct: 2
     }
@@ -58,6 +58,9 @@ let showResult = false;
 let IsApproved = false;
 let selectedAnswer = null;
 
+
+let nodeList = document.querySelectorAll("input");
+let errorMsg = document.getElementById("user-error-message")
 // function showResult() 
 // {
 //     if(currentQuestion > 7) 
@@ -74,30 +77,101 @@ function IsQuizApproved(points) {
     else {
         //Icke godkänd
     }
+
+}
+
+function resetState() {
+    selectedAnswer = null;
+
+    nodeList.forEach(input => {
+        let currentDiv = input.closest('.questions-container');
+        input.disabled = false;
+        input.checked = false;
+        currentDiv.classList.remove("correct");
+        currentDiv.classList.remove("wrong");
+        errorMsg.textContent = "";
+    })
+}
+
+function correctAnswer(currentQuestion, selectedAnswer) {
+
+    let radioInput = document.getElementById(selectedAnswer);
+
+
+    nodeList.forEach(input => {
+        let currentDiv = input.closest('.questions-container');
+
+
+        if (questions[currentQuestion].correct == input.id) {
+
+            //document.getElementsByClass(".correct")
+            currentDiv.classList.add("correct")
+            assignPoints(currentQuestion, selectedAnswer)
+
+        } else {
+            currentDiv.classList.add("wrong")
+        }
+        input.disabled = true;
+    })
+
+
+
+
+
+    //     let radioInput= document.getElementById(selectedAnswer);
+
+    // // const isCorrect= selectedAnswer.questions.correct===
+    // if (questions[currentQuestion].correct == selectedAnswer) {
+    //     radioInput.closest('.questions-container').classList.add("correct")
+    //     points++
+    // }
+
+
+    // else {
+    //   radioInput.closest('.questions-container').classList.add("wrong");
+    // }
+    // let nodeList = document.querySelectorAll("input")
+
+    // nodeList.forEach(input => {
+    //     input.disabled = true;
+    //     console.log(input)
+    // })
+}
+
+function assignPoints(currentQuestion, selectedAnswer) {
+    if (questions[currentQuestion].correct == selectedAnswer) {
+
+        points++
+        console.log(points)
+    }
 }
 function userSelectedAnswer() {
     document.querySelector(".question-area").addEventListener('click', function (event) {
         if (event.target.checked === true) {
-           selectedAnswer = event.target.id;
-           console.log(event.target.id);
-           console.log(selectedAnswer);
+            selectedAnswer = event.target.id;
+            correctAnswer(showCurrentQuestion, selectedAnswer);
         }
-    console.log(event.target.checked)
-   });
+    });
 }
+
 function nextQuestion() {
-    let button = document.getElementById("next-question").addEventListener("click", function () {
+    document.getElementById("next-question").addEventListener("click", function () {
         if (selectedAnswer === null) {
-   document.getElementById("user-error-message").textContent = "*Du måste välja ett svarsalternativ";
-  
-}
-// else if() { 
-// showCurrentQuestion++
-// showQuestion();
-// console.log(showCurrentQuestion)
-// }
- })
-   
+            errorMsg.textContent = "*Du måste välja ett svarsalternativ";
+        } else {
+            resetState();
+            showCurrentQuestion++;
+            showQuestion(showCurrentQuestion)
+            console.log("test next question button")
+
+        }
+        // else if() { 
+        // showCurrentQuestion++
+        // showQuestion();
+        // console.log(showCurrentQuestion)
+        // }
+    })
+
 
     //if (currentQuestionIndex < questions.length) {
     //     visa nästa fråga
@@ -108,28 +182,33 @@ function nextQuestion() {
 }
 
 
-function showQuestion(showCurrentQuestion) {
-    const currentQuestion = questions[showCurrentQuestion].question;
-    console.log(currentQuestion);
+function showQuestion(questionIndex) {
+    if (questionIndex <= questions.length - 1) {
+        const QuestionHeaderText = `${questions[questionIndex].question} av ${questions.length}`;
+        const showAnswer1 = questions[questionIndex].answers[0];
+        const showAnswer2 = questions[questionIndex].answers[1];
+        const showAnswer3 = questions[questionIndex].answers[2];
+        const showAnswer4 = questions[questionIndex].answers[3];
 
-    const showAnswer1 = questions[showCurrentQuestion].answers[0];
-    const showAnswer2 = questions[showCurrentQuestion].answers[1];
-    const showAnswer3 = questions[showCurrentQuestion].answers[2];
-    const showAnswer4 = questions[showCurrentQuestion].answers[3];
+        // Frågan
+        document.getElementById("question-text").textContent = QuestionHeaderText;
 
-    var testtext = document.getElementById("question-text").textContent = currentQuestion;
+        //Svaren
+        document.getElementById("answer-text-1").textContent = showAnswer1;
+        document.getElementById("answer-text-2").textContent = showAnswer2;
+        document.getElementById("answer-text-3").textContent = showAnswer3;
+        document.getElementById("answer-text-4").textContent = showAnswer4;
+    } else {
+        // console.log("final result");
 
-    document.getElementById("answer-text-1").textContent = showAnswer1;
-    document.getElementById("answer-text-2").textContent = showAnswer2;
-    document.getElementById("answer-text-3").textContent = showAnswer3;
-    document.getElementById("answer-text-4").textContent = showAnswer4;
-
+        // ta bort frågorna och svaren
+        // visa final points och om dem blivit godkända
+    }
 }
 //skapa en funktion för att räkna poäng
 //spara i LocalStorage för att behålla statet och vilketn fråga de är på samt hur många poäng man har.
 //När vi bockar i ett svar ska rätt svar komma upp (och om man har svarat rätt eller fel)
 
 showQuestion(showCurrentQuestion);
-console.log(questions);
 nextQuestion();
 userSelectedAnswer();
